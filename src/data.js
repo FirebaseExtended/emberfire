69c20ec63737427f7826be13a66ee708acababae
Adam Putinski <adam.putinski@gmail.com>
2014-03-17 09:24:03 -0700

    Added serializer

--- src/data.js
+++ src/data.js
@@ -8,6 +8,90 @@
   }
 
   /**
+    Something about the serializer
+  */
+  DS.FirebaseSerializer = DS.JSONSerializer.extend(Ember.Evented, {
+
+    /**
+      Called after `extractSingle()`. This method checks the model
+      for `hasMany` relationships and makes sure the value is an array.
+      If the value is an object, return the keys as an array.
+    */
+    normalize: function(type, hash) {
+      var relationshipsByName = Ember.get(type, 'relationshipsByName');
+      var relationshipNames = Ember.get(type, 'relationshipNames');
+      // Check if the model contains any 'hasMany' relationships
+      if (Ember.isArray(relationshipNames.hasMany)) {
+        relationshipNames.hasMany.forEach(function (key) {
+          var relationship = relationshipsByName.get(key);
+          relationship.options.serializeAs = 'object';
+          // Return the keys as an array
+          if (typeof hash[key] === 'object' && !Ember.isArray(hash[key])) {
+            hash[key] = Ember.keys(hash[key]);
+            /*if (relationship.options && typeof relationship.options.serializeAs === 'undefined') {
+              relationship.options.serializeAs = 'object';
+            }*/
+          }
+          else if (Ember.isArray(hash[key])) {
+            /*if (relationship.options && typeof relationship.options.serializeAs === 'undefined') {
+              relationship.options.serializeAs = 'array';
+            }*/
+            Ember.warn('%@ relationship %@(\'%@\') will scale better if represented as a key/value map in Firebase. Example: {"%@":{"%@_id":true}`}'.fmt(type.toString(), relationship.kind, relationship.type.typeKey, relationship.key, relationship.type.typeKey));
+            //throw new Error('%@ relationship %@(\'%@\') must be a key/value map in Firebase. Example: { "%@": { "%@_id": true } }'.fmt(type.toString(), relationship.kind, relationship.type.typeKey, relationship.key, relationship.type.typeKey));
+          }
+        });
+      }
+      return this._super.apply(this, arguments);
+    },
+
+    /**
+      extractSingle
+    */
+    extractSingle: function(store, type, payload) {
+      return this.normalize(type, payload);
+    },
+
+    /**
+      Called after the adpter runs `findAll()` or `findMany()`. This method runs
+      `extractSingle()` on each item in the payload and as a result each item
+      will have `normalize()` called on it
+    */
+    extractArray: function(store, type, payload) {
+      return payload.map(function(item) {
+        return this.extractSingle(store, type, item);
+      }.bind(this));
+    },
+
+    /**
+      serializeHasMany
+    */
+    serializeHasMany: function(record, json, relationship) {
+      var key = relationship.key;
+      var relationshipType = DS.RelationshipChange.determineRelationshipType(record.constructor, relationship);
+
+      if (record.get(key).get('length') === 0) { return; }
+
+      if (relationship.kind === 'hasMany') {
+        if (relationship.options) {
+          relationship.options.serializeAs = relationship.options.serializeAs || 'array';
+          switch (relationship.options.serializeAs) {
+            case 'array':
+              json[key] = record.get(key).mapBy('id');
+              break;
+            case 'object':
+              json[key] = record.get(key).reduce(function(obj, item, idx) {
+                obj[item.get('id')] = true;
+                return obj;
+              }, {});
+              break;
+          }
+        }
+      }
+    }
+
+  });
+
+  /**
     The Firebase adapter allows your store to communicate with the Firebase
     realtime service. To use the adapter in your app, extend DS.FirebaseAdapter
     and customize the endpoint to point to the Firebase URL where you want this
@@ -19,25 +103,26 @@
     otherwise.
   */
   DS.FirebaseAdapter = DS.Adapter.extend(Ember.Evented, {
+
     /**
       Endpoint paths can be customized by setting the Firebase property on the
       adapter:
 
       ```js
-      DS.FirebaseAdapter.reopen({
-        firebase: new Firebase("https://<my-firebase>.firebaseio.com/")
+      DS.FirebaseAdapter.extend({
+        firebase: new Firebase('https://<my-firebase>.firebaseio.com/')
       });
       ```
 
-      Requests for `App.Post` would now target `/post`.
+      Requests for `App.Post` would now target `https://<my-firebase>.firebaseio.com/posts`.
 
       @property firebase
       @type {Firebase}
     */
 
     init: function() {
-      if (!this.firebase || typeof this.firebase != "object") {
-        throw new Error("Please set the `firebase` property on the adapter.");
+      if (!this.firebase || typeof this.firebase !== 'object') {
+        throw new Error('Please set the `firebase` property on the adapter.');
       }
       // If provided Firebase reference was a query (eg: limits), make it a ref.
       this._ref = this.firebase.ref();
@@ -59,8 +144,10 @@
     find: function(store, type, id) {
       var resolved = false;
       var ref = this._getRef(type, id);
+      var serializer = store.serializerFor(type);
+
       return new Ember.RSVP.Promise(function(resolve, reject) {
-        ref.on("value", function(snapshot) {
+        ref.on('value', function(snapshot) {
           var obj = snapshot.val();
           if (obj) {
             obj.id = snapshot.name();
@@ -70,8 +157,14 @@
             resolved = true;
             Ember.run(null, resolve, obj);
           } else {
-            // Otherwise, update the store.
-            store.push(type, obj);
+            // If the snapshot is null, delete the record from the store
+            if (obj === null && store.hasRecordForId(type, snapshot.name())) {
+              store.getById(type, snapshot.name()).destroyRecord();
+            }
+            // Otherwise push it into the store
+            else {
+              store.push(type, serializer.extractSingle(store, type, obj));
+            }
           }
         }, function(err) {
           // Only called in cases of permission related errors.
@@ -79,7 +172,7 @@
             Ember.run(null, reject, err);
           }
         });
-      }, "DS: FirebaseAdapter#find " + type + " to " + ref.toString());
+      }, 'DS: FirebaseAdapter#find ' + type + ' to ' + ref.toString());
     },
 
     /**
@@ -94,6 +187,7 @@
     findAll: function(store, type) {
       var resolved = false;
       var ref = this._getRef(type);
+      var serializer = store.serializerFor(type);
 
       function _gotChildValue(snapshot) {
         // Update store, only if the promise is already resolved.
@@ -102,20 +196,20 @@
         }
         var obj = snapshot.val();
         obj.id = snapshot.name();
-        store.push(type, obj);
+        store.push(type, serializer.extractSingle(store, type, obj));
       }
 
       return new Ember.RSVP.Promise(function(resolve, reject) {
-        function _handleError(err) {
+        var _handleError = function(err) {
           if (!resolved) {
             resolved = true;
             Ember.run(null, reject, err);
           }
-        }
+        };
 
-        ref.on("child_added", _gotChildValue, _handleError);
-        ref.on("child_changed", _gotChildValue, _handleError);
-        ref.on("child_removed", function(snapshot) {
+        ref.on('child_added', _gotChildValue, _handleError);
+        ref.on('child_changed', _gotChildValue, _handleError);
+        ref.on('child_removed', function(snapshot) {
           if (!resolved) {
             return;
           }
@@ -125,7 +219,7 @@
           }
         }, _handleError);
 
-        ref.once("value", function(snapshot) {
+        ref.once('value', function(snapshot) {
           var results = [];
           snapshot.forEach(function(child) {
             var record = child.val();
@@ -135,7 +229,7 @@
           resolved = true;
           Ember.run(null, resolve, results);
         });
-      }, "DS: FirebaseAdapter#findAll " + type + " to " + ref.toString());
+      }, 'DS: FirebaseAdapter#findAll ' + type + ' to ' + ref.toString());
     },
 
     /**
@@ -147,7 +241,7 @@
       been successfully saved to Firebase.
     */
     createRecord: function(store, type, record) {
-      var data = record.serialize({includeId: false});
+      var data = record.serialize({ includeId: false });
       var ref = this._getRef(type, record.id);
       return new Ember.RSVP.Promise(function(resolve, reject) {
         ref.set(data, function(err) {
@@ -157,7 +251,7 @@
             Ember.run(null, resolve);
           }
         });
-      }, "DS: FirebaseAdapter#createRecord " + type + " to " + ref.toString());
+      }, 'DS: FirebaseAdapter#createRecord ' + type + ' to ' + ref.toString());
     },
 
     /**
@@ -165,7 +259,17 @@
       attributes for a given model don't change.
     */
     updateRecord: function(store, type, record) {
-      return this.createRecord(store, type, record);
+      var data = record.serialize({ includeId: false });
+      var ref = this._getRef(type, record.id);
+      return new Ember.RSVP.Promise(function(resolve, reject) {
+        ref.update(data, function(err) {
+          if (err) {
+            Ember.run(null, reject, err);
+          } else {
+            Ember.run(null, resolve);
+          }
+        });
+      }, 'DS: FirebaseAdapter#updateRecord ' + type + ' to ' + ref.toString());
     },
 
     // Called by the store when a record is deleted.
@@ -179,7 +283,7 @@
             Ember.run(null, resolve);
           }
         });
-      }, "DS: FirebaseAdapter#deleteRecord " + type + " to " + ref.toString());
+      }, 'DS: FirebaseAdapter#deleteRecord ' + type + ' to ' + ref.toString());
     },
 
     /**
