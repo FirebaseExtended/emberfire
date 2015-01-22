1639e519ad6c87dc41e382f4fb5717ac3d5c9a6b
Tim Stirrat <tim.stirrat@gmail.com>
2015-01-21 18:45:29 -0800

    basic findQuery support

--- addon/adapters/firebase.js
+++ addon/adapters/firebase.js
@@ -185,6 +185,57 @@ export default DS.Adapter.extend(Ember.Evented, {
     }, fmt('DS: FirebaseAdapter#findAll %@ to %@', [type, ref.toString()]));
   },
 
+  findQuery: function(store, type, query) {
+    var adapter = this;
+    var ref = this._getRef(type);
+
+    if (!query.orderBy || query.orderBy === '_key') {
+      ref = ref.orderByKey();
+    } else if (query.orderBy && query.orderBy !== '_priority') {
+      ref = ref.orderByChild(query.orderBy);
+    } else {
+      ref = ref.orderByPriority();
+    }
+
+    if (query.limitToFirst && query.limitToFirst >= 0) {
+      ref = ref.limitToFirst(query.limitToFirst);
+    }
+
+    if (query.limitToLast && query.limitToLast >= 0) {
+      ref = ref.limitToLast(query.limitToLast);
+    }
+
+    if (query.startAt) {
+      ref = ref.startAt(query.startAt);
+    }
+
+    if (query.endAt) {
+      ref = ref.endAt(query.endAt);
+    }
+
+    if (query.equalTo) {
+      ref = ref.equalTo(query.equalTo);
+    }
+
+    return new Promise(function(resolve, reject) {
+      // Listen for child events on the type
+      ref.once('value', function(snapshot) {
+        if (!adapter._findAllHasEventsForType(type)) {
+          adapter._findAllAddEventListeners(store, type, ref);
+        }
+        var results = [];
+        snapshot.forEach(function(childSnapshot) {
+          var payload = adapter._assignIdToPayload(childSnapshot);
+          adapter._updateRecordCacheForType(type, payload);
+          results.push(payload);
+        });
+        resolve(results);
+      }, function(error) {
+        reject(error);
+      });
+    }, fmt('DS: FirebaseAdapter#findQuery %@ with %@', [type, query]));
+  },
+
   /**
     Keep track of what types `.findAll()` has been called for
     so duplicate listeners aren't added
