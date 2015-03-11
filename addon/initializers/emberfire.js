e6d326ae55f2e9c256a8f509d3be11759bd5eb31
Firebase Operations <operations@firebase.com>
2015-03-11 04:08:11 +0000

    [firebase-release] Updated EmberFire to 1.4.2

--- addon/initializers/emberfire.js
+++ addon/initializers/emberfire.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 import FirebaseAdapter from '../adapters/firebase';
 import FirebaseSerializer from '../serializers/firebase';
 
-var VERSION = '0.0.0';
+var VERSION = '1.4.2';
 
 if (Ember.libraries) {
   Ember.libraries.registerCoreLibrary('EmberFire', VERSION);
