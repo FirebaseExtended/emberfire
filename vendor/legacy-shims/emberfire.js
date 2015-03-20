ede80d78e114e9f11cbeb7a7365f2f6699b57e3a
Firebase Operations <operations@firebase.com>
2015-03-20 16:45:58 +0000

    [firebase-release] Updated EmberFire to 1.4.3

--- vendor/legacy-shims/emberfire.js
+++ vendor/legacy-shims/emberfire.js
@@ -8,7 +8,7 @@ import EmberFireInitializer from '../../addon/initializers/emberfire';
  * Ember Data. The DS.FirebaseAdapter provides all of the standard DS.Adapter
  * methods and will automatically synchronize the store with Firebase.
  *
- * EmberFire 0.0.0
+ * EmberFire 1.4.3
  * https://github.com/firebase/emberfire/
  * License: MIT
  */
