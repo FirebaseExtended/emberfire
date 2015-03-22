9828eb1d12a1e16f0e889351a7ec91d4e7ae44d2
Tim Stirrat <tstirrat@firebase.com>
2015-03-22 09:37:21 -0700

    Correctly add header comment to TOP of file

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ var transpile  = require('gulp-es6-module-transpiler');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var jshint = require('gulp-jshint');
+var header = require('gulp-header');
+var fs = require('fs');
 
 gulp.task('lint', function () {
   return gulp.src('{addon,app,config,tests}/**/*.js')
@@ -29,6 +31,7 @@ gulp.task('build-legacy', ['lint'], function() {
       formatter: 'bundle'
     }))
     .pipe(concat('emberfire.js'))
+    .pipe(header(fs.readFileSync('vendor/legacy-shims/header.js', 'utf8')))
     .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist'));
 });
@@ -40,6 +43,7 @@ gulp.task('build-legacy-minified', ['lint'], function() {
       formatter: 'bundle'
     }))
     .pipe(concat('emberfire.min.js'))
+    .pipe(header(fs.readFileSync('vendor/legacy-shims/header.js', 'utf8')))
     .pipe(uglify({
       preserveComments: 'some'
     }))
