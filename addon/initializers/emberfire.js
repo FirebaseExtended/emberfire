dec9fdf6af94a25dd88562588571a0c7acaf4e21
Tim Stirrat <tstirrat@firebase.com>
2015-03-31 14:57:37 -0700

    Only monkeypatch the store once
    Was causing a new level of super wrappers for every `startApp()`
    in the tests :)

--- addon/initializers/emberfire.js
+++ addon/initializers/emberfire.js
@@ -22,41 +22,47 @@ export default {
     app.register('serializer:-firebase', FirebaseSerializer);
 
     // Monkeypatch the store until ED gives us a good way to listen to push events
-    DS.Store.reopen({
-      push: function(typeName, data, _partial) {
-        var record = this._super(typeName, data, _partial);
-        var adapter = this.adapterFor(record.constructor);
-        if (adapter.recordWasPushed) {
-          adapter.recordWasPushed(this, typeName, record);
-        }
-        return record;
-      },
+    if (!DS.Store.prototype._emberfirePatched) {
+      DS.Store.reopen({
+        _emberfirePatched: true,
+        push: function(typeName, data, _partial) {
+          var record = this._super(typeName, data, _partial);
+          var adapter = this.adapterFor(record.constructor);
+          if (adapter.recordWasPushed) {
+            adapter.recordWasPushed(this, typeName, record);
+          }
+          return record;
+        },
 
-      recordWillUnload: function(record) {
-        var adapter = this.adapterFor(record.constructor);
-        if (adapter.recordWillUnload) {
-          adapter.recordWillUnload(this, record);
-        }
-      },
+        recordWillUnload: function(record) {
+          var adapter = this.adapterFor(record.constructor);
+          if (adapter.recordWillUnload) {
+            adapter.recordWillUnload(this, record);
+          }
+        },
 
-      recordWillDelete: function (record) {
-        var adapter = this.adapterFor(record.constructor);
-        if (adapter.recordWillDelete) {
-          adapter.recordWillDelete(this, record);
+        recordWillDelete: function (record) {
+          var adapter = this.adapterFor(record.constructor);
+          if (adapter.recordWillDelete) {
+            adapter.recordWillDelete(this, record);
+          }
         }
-      }
-    });
+      });
+    }
 
-    DS.Model.reopen({
-      unloadRecord: function() {
-        this.store.recordWillUnload(this);
-        return this._super();
-      },
-      deleteRecord: function () {
-        this.store.recordWillDelete(this);
-        this._super();
-      }
-    });
+    if (!DS.Model.prototype._emberfirePatched) {
+      DS.Model.reopen({
+        _emberfirePatched: true,
+        unloadRecord: function() {
+          this.store.recordWillUnload(this);
+          return this._super();
+        },
+        deleteRecord: function () {
+          this.store.recordWillDelete(this);
+          this._super();
+        }
+      });
+    }
 
     DS.FirebaseAdapter = FirebaseAdapter;
     DS.FirebaseSerializer = FirebaseSerializer;
