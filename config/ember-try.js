3f97d2d51169450558c6956f4b38f6573d29c775
Tim Stirrat <tstirrat@google.com>
2015-06-04 13:20:45 -0700

    Test against more ember-data versions

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,8 +1,40 @@
 module.exports = {
   scenarios: [
     {
-      name: 'default',
-      dependencies: { }
+      name: 'ember-data-beta.12',
+      dependencies: {
+        'ember-data': '1.0.0-beta.12'
+      },
+      resolutions: {
+        'ember-data': '1.0.0-beta.12'
+      }
+    },
+    {
+      name: 'ember-data-beta.14.1',
+      dependencies: {
+        'ember-data': '1.0.0-beta.14.1'
+      },
+      resolutions: {
+        'ember-data': '1.0.0-beta.14.1'
+      }
+    },
+    {
+      name: 'ember-data-beta.16.1',
+      dependencies: {
+        'ember-data': '1.0.0-beta.16.1'
+      },
+      resolutions: {
+        'ember-data': '1.0.0-beta.16.1'
+      }
+    },
+    {
+      name: 'ember-data-beta.17',
+      dependencies: {
+        'ember-data': '1.0.0-beta.17'
+      },
+      resolutions: {
+        'ember-data': '1.0.0-beta.17'
+      }
     },
     {
       name: 'ember-data-release',
@@ -11,15 +43,6 @@ module.exports = {
       },
       resolutions: {
         'ember-data': 'release'
-      }
-    },
-    {
-      name: 'ember-data-beta',
-      dependencies: {
-        'ember-data': 'components/ember-data#beta'
-      },
-      resolutions: {
-        'ember-data': 'beta'
       }
     },
     {
