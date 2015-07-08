b33ac5ca657480a75457a9a150243bb08906c166
Tim Stirrat <tstirrat@google.com>
2015-07-08 11:30:27 -0700

    Use browserify to create legacy js libs

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var gulp = require('gulp');
 
 var del = require('del');
 var $ = require('gulp-load-plugins')();
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
 var fs = require('fs');
 
 gulp.task('lint', function () {
@@ -12,40 +15,34 @@ gulp.task('lint', function () {
 });
 
 gulp.task('clean-dist', function (cb) {
-  del([
-    'dist/',
-    'dist/tmp',
-  ], cb);
+  del(['dist/'], cb);
 });
 
 gulp.task('build-legacy', ['lint'], function() {
-  return gulp.src('vendor/legacy-shims/emberfire.js')
-    .pipe($.sourcemaps.init())
-    .pipe($.es6ModuleTranspiler({
-      importPaths: ['vendor/legacy-shims'],
-      formatter: 'bundle'
-    }))
-    .pipe($.concat('emberfire.js'))
-    .pipe($.header(fs.readFileSync('vendor/legacy-shims/header.js', 'utf8')))
+  var b = browserify(['vendor/legacy/emberfire.js'], {
+    debug: true
+  });
+
+  return b.bundle()
+    .pipe(source('emberfire.js'))
+    .pipe(buffer())
+    .pipe($.sourcemaps.init({ loadMaps: true }))
+      .pipe($.header(fs.readFileSync('vendor/legacy/header.js', 'utf8')))
     .pipe($.sourcemaps.write('./'))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build-legacy-minified', ['lint'], function() {
-  return gulp.src('vendor/legacy-shims/emberfire.js')
-    .pipe($.es6ModuleTranspiler({
-      importPaths: ['vendor/legacy-shims'], // for 'ember' and 'ember-data' global shims
-      formatter: 'bundle'
-    }))
-    .pipe($.concat('emberfire.min.js'))
+gulp.task('build-legacy-minified', ['build-legacy'], function() {
+  return gulp.src('dist/emberfire.js')
+    .pipe($.rename('emberfire.min.js'))
     .pipe($.uglify())
     .on('error', function (e) {
       throw new $.util.PluginError('gulp-uglify', e.message);
     })
-    .pipe($.header(fs.readFileSync('vendor/legacy-shims/header.js', 'utf8')))
+    .pipe($.header(fs.readFileSync('vendor/legacy/header.js', 'utf8')))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('legacy', ['clean-dist', 'build-legacy', 'build-legacy-minified']);
+gulp.task('legacy', ['clean-dist', 'build-legacy-minified']);
 
 gulp.task('default', ['legacy']);
--- vendor/legacy/emberfire.js
+++ vendor/legacy/emberfire.js
@@ -1,10 +1,11 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 import FirebaseAdapter from '../../addon/adapters/firebase';
 import FirebaseSerializer from '../../addon/serializers/firebase';
 import EmberFireInitializer from '../../addon/initializers/emberfire';
 
-window.DS.FirebaseAdapter = FirebaseAdapter;
-window.DS.FirebaseSerializer = FirebaseSerializer;
+DS.FirebaseAdapter = FirebaseAdapter;
+DS.FirebaseSerializer = FirebaseSerializer;
 
 Ember.onLoad('Ember.Application', function(Application) {
   Application.initializer(EmberFireInitializer);
