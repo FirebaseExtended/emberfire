55559877e8ad653147b68c71aba8abb4abf8f6ec
Tim Stirrat <tstirrat@google.com>
2015-07-28 09:02:12 -0700

    Add 'password' auth to torii-provider

--- addon/torii-providers/firebase.js
+++ addon/torii-providers/firebase.js
@@ -6,14 +6,40 @@ export default Ember.Object.extend({
   open: function(options) {
     var self = this;
 
-    return new Ember.RSVP.Promise(function(resolve, reject) {
-      self.get('firebase').authWithOAuthPopup(options.authWith, function(error, authData) {
-        if (error) {
-          reject(error);
-        } else {
-          resolve(authData);
+    var provider = options.authWith || options.provider;
+
+    return new Ember.RSVP.Promise((resolve, reject) => {
+      if (!provider) {
+        reject(new Error('`provider` must be supplied'));
+      }
+
+      if (provider === 'password') {
+        if (!options.email && !options.password) {
+          reject(new Error('`email` and `password` must be supplied'));
         }
-      });
+
+        this.get('firebase').authWithPassword({
+          email: options.email,
+          password: options.password,
+        }, (error, authData) => {
+
+          if (error) {
+            reject(error);
+          } else {
+            resolve(authData);
+          }
+        });
+
+      } else {
+        this.get('firebase').authWithOAuthPopup(provider, (error, authData) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(authData);
+          }
+        });
+      }
+
     });
   }
 });
