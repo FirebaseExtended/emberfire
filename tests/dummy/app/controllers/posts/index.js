422986dbbf426fc516db4e4040738177c35d9f0c
Tim Stirrat <tstirrat@google.com>
2015-07-31 10:33:24 -0700

    Correctly sort dummy app posts

--- tests/dummy/app/controllers/posts/index.js
+++ tests/dummy/app/controllers/posts/index.js
@@ -1,3 +1,7 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend();
+export default Ember.Controller.extend({
+  posts: Ember.computed.sort('model', function (a, b) {
+    return b.get('published') - a.get('published');
+  })
+});
