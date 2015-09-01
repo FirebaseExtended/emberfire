6c6ac39fb04d67a86510cbca951990ce8fc42140
Leif Ringstad <leifcr@gmail.com>
2015-09-01 22:36:28 +0200

    Replace Ember.Handlebars.SafeString with Ember.String.htmlSafe

--- tests/dummy/app/helpers/breaklines.js
+++ tests/dummy/app/helpers/breaklines.js
@@ -3,5 +3,5 @@ import Ember from 'ember';
 export default function(value, options) {
   var escaped = Ember.Handlebars.Utils.escapeExpression(value);
       escaped = escaped.replace(/(\r\n|\n|\r)/gm, '<br>');
-  return new Ember.Handlebars.SafeString(escaped);
+  return new Ember.String.htmlSafe(escaped);
 }
--- tests/dummy/app/helpers/markdown.js
+++ tests/dummy/app/helpers/markdown.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default function(value, options) {
   if (value) {
-    return new Ember.Handlebars.SafeString(window.markdown.toHTML(value));
+    return new Ember.String.htmlSafe(window.markdown.toHTML(value));
   }
 }
