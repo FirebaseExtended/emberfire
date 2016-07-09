6d9545ccc04528d8d4036ae1fb947eda7a80cce4
Ben Holmes <mail@benholmes.uk>
2016-07-09 13:33:34 -0700

    Add an example helper module for emberfire acceptance tests

---
+++ blueprints/emberfire/files/tests/helpers/module-for-emberfire-acceptance.js
@@ -0,0 +1,29 @@
+import moduleForAcceptance from './module-for-acceptance';
+
+import createOfflineRef from './create-offline-ref';
+import replaceAppRef from './replace-app-ref';
+import stubFirebase from './stub-firebase';
+import unstubFirebase from './unstub-firebase';
+
+export default function(name, options = {}) {
+  let fixtureData = options.fixtureData || {};
+  moduleForAcceptance(name, {
+    beforeEach() {
+      stubFirebase();
+      this.firebaseReference = createOfflineRef(fixtureData);
+      replaceAppRef(this.application, this.firebaseReference);
+
+      if (options.beforeEach) {
+        options.beforeEach.apply(this, arguments);
+      }
+    },
+
+    afterEach() {
+      if (options.afterEach) {
+        options.afterEach.apply(this, arguments);
+      }
+
+      unstubFirebase();
+    }
+  });
+}
