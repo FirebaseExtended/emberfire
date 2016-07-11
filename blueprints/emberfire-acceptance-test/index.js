dd58be420843f6bb0d412934ee05e66c670c81bb
Ben Holmes <mail@benholmes.uk>
2016-07-11 12:33:51 -0700

    Define logic for generating qunit or mocha type tests

--- blueprints/emberfire-acceptance-test/index.js
+++ blueprints/emberfire-acceptance-test/index.js
@@ -1,7 +1,27 @@
 /*jshint node:true*/
 
 var acceptanceBlueprint = require('ember-cli/blueprints/acceptance-test');
+var mochaDefined = require('../')
+var path = require('path');
 
-acceptanceBlueprint.description = "Generates an acceptance test for a Firebase backed feature.";
+var emberfireAcceptanceBlueprint = {
+  description: 'Generates an acceptance test for a Firebase backed feature.',
 
-module.exports = acceptanceBlueprint;
+  filesPath() {
+    var blueprintPath = 'qunit-files';
+
+    // console.log(this.project);
+    if (this.project && this.project.addons) {
+      var addonNames = this.project.addons.map(function(addon) {
+        return addon.name;
+      });
+
+      if (addonNames.indexOf('ember-cli-mocha') === -1) {
+        blueprintPath = 'mocha-files';
+      }
+    }
+    return(path.join(this.path, blueprintPath));
+  }
+};
+
+module.exports = Object.assign({}, acceptanceBlueprint, emberfireAcceptanceBlueprint);
