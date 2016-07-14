951d986f8a0513ee52ae726548f08bef676a859b
Ben Holmes <mail@benholmes.uk>
2016-07-14 16:30:51 -0700

    Determine the local variables by whether we are using mocha

--- blueprints/emberfire-acceptance-test/index.js
+++ blueprints/emberfire-acceptance-test/index.js
@@ -2,31 +2,32 @@
 
 var path = require('path');
 
-var acceptanceBlueprint;
+var mochaBlueprint = require('ember-cli-mocha/blueprints/acceptance-test');
+var acceptanceBlueprint = require('ember-cli/blueprints/acceptance-test');
 
-try {
-  acceptanceBlueprint = require('ember-cli-mocha/blueprints/acceptance-test');
-} catch(e) {
-  acceptanceBlueprint = require('ember-cli/blueprints/acceptance-test');
-}
+var isMochaProject = function(project) {
+  if (project && project.addons) {
+    return project.addons.map((addon) => addon.name).indexOf('ember-cli-mocha') !== -1;
+  }
+};
 
 var emberfireAcceptanceBlueprint = {
   description: 'Generates an acceptance test for a Firebase backed feature.',
 
+  locals: function() {
+    blueprint = isMochaProject(this.project) ? mochaBlueprint : acceptanceBlueprint;
+    return blueprint.locals.apply(this, arguments)
+  },
+
   filesPath: function() {
     var blueprintPath = 'qunit-files';
 
-    if (this.project && this.project.addons) {
-      var addonNames = this.project.addons.map(function(addon) {
-        return addon.name;
-      });
-
-      if (addonNames.indexOf('ember-cli-mocha') === -1) {
-        blueprintPath = 'mocha-files';
-      }
+    if (isMochaProject(this.project)) {
+      blueprintPath = 'mocha-files';
     }
+
     return(path.join(this.path, blueprintPath));
   }
 };
 
-module.exports = Object.assign({}, acceptanceBlueprint, emberfireAcceptanceBlueprint);
+module.exports = Object.assign({}, acceptanceBlueprint, mochaBlueprint, emberfireAcceptanceBlueprint);
--- blueprints/emberfire-acceptance-test/mocha-files/tests/acceptance/__name__-test.js
+++ blueprints/emberfire-acceptance-test/mocha-files/tests/acceptance/__name__-test.js
@@ -2,8 +2,6 @@
 import { it } from 'mocha';
 import { expect } from 'chai';
 import describeEmberfireAcceptance from '../helpers/describe-emberfire-acceptance';
-import startApp from '../helpers/start-app';
-<% if (destroyAppExists) { %>import destroyApp from '../helpers/destroy-app';<% } else { %>import Ember from 'ember';<% } %>
 
 describeEmberfireAcceptance('Acceptance: <%= classifiedModuleName %>', function() {
   it('can visit /<%= dasherizedModuleName %>', function() {
