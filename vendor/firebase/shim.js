5e0d56536aac8074dd98c7c7d7b552b315fd0b41
Robert Wagner <rwwagner90@gmail.com>
2016-08-31 10:17:56 -0400

    Remove `to` capitalize FakeFirebase

--- vendor/firebase/shim.js
+++ vendor/firebase/shim.js
@@ -1,17 +1,15 @@
 /* globals Firebase */
 
 (function() {
-  function fakeFirebase() {
+  function FakeFirebase() {
     var error = "Firebase is not available. Either the bower package is missing or your app is in Fastboot Mode.";
-    this.to = function() {
-      throw Error(error);
-    };
+
     throw Error(error);
   }
 
   function vendorModule() {
     'use strict';
-    return {'default': self['firebase'] || fakeFirebase};
+    return {'default': self['firebase'] || FakeFirebase};
   }
 
   define('firebase', [], vendorModule);
