333fc8b443038ce5463f874654fa066782d83b23
James Daniels <jamesdaniels@google.com>
2018-06-25 12:41:02 -0700

    Cleanup firebase-app service

--- addon/services/firebase-app.ts
+++ addon/services/firebase-app.ts
@@ -2,14 +2,12 @@ import Service from '@ember/service';
 import { get, set } from '@ember/object';
 import { inject as service } from '@ember/service';
 
-// TODO no regsiterAuth...
-import {  } from  '@firebase/auth';
-import { registerDatabase } from '@firebase/database';
-// @ts-ignore TODO missing types
-import { registerFirestore } from '@firebase/firestore';
-import { registerFunctions } from '@firebase/functions';
-import { registerMessaging } from '@firebase/messaging';
-import { registerStorage } from '@firebase/storage';
+import 'firebase/auth';
+import 'firebase/database';
+import 'firebase/firestore';
+import 'firebase/functions';
+import 'firebase/messaging';
+import 'firebase/storage';
 
 import { app, auth, database, firestore, functions, messaging, storage } from 'firebase';
 
@@ -28,34 +26,12 @@ export default class FirebaseApp extends Service.extend({
 }) {
 
     options?: {[key:string]: any};
-    auth = (): auth.Auth => {
-        return getApp(this).auth();
-    };
-    database = (databaseURL?: string): database.Database => {
-        const app: any = getApp(this);
-        if (app.database === undefined) { registerDatabase(app.firebase_) }
-        return app.database(databaseURL);
-    };
-    firestore = (): firestore.Firestore => {
-        const app: any = getApp(this);
-        if (app.firestore === undefined) { registerFirestore(app.firebase_) }
-        return app.firestore();
-    }
-    functions = (): functions.Functions => {
-        const app: any = getApp(this);
-        if (app.functions === undefined) { registerFunctions(app.firebase_) }
-        return app.functions();
-    }
-    messaging = (): messaging.Messaging => {
-        const app: any = getApp(this);
-        if (app.messaging === undefined) { registerMessaging(app.firebase_) }
-        return app.messaging();
-    }
-    storage = (storageBucket?: string): storage.Storage => {
-        const app: any = getApp(this);
-        if (app.storage === undefined) { registerStorage(app.firebase_) }
-        return app.storage(storageBucket);
-    }
+    auth = (): auth.Auth => getApp(this).auth();
+    database = (databaseURL?: string): database.Database => (getApp(this).database as any)(databaseURL);
+    firestore = (): firestore.Firestore => getApp(this).firestore();
+    functions = (): functions.Functions => getApp(this).functions();
+    messaging = (): messaging.Messaging => getApp(this).messaging();
+    storage = (storageBucket?: string): storage.Storage => getApp(this).storage(storageBucket);
 
     init() {
         this._super(...arguments);
