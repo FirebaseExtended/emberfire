09b2b2ca82e74f4db15b8d50218232deffe2d881
James Daniels <james@jamesdaniels.net>
2019-10-06 01:13:49 +0100

    Missing argument on the dummy route

--- tests/dummy/app/routes/comment.js
+++ tests/dummy/app/routes/comment.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 import RealtimeRouteMixin from 'emberfire/mixins/realtime-route';
 
 export default Route.extend(RealtimeRouteMixin, {
-    model() {
+    model(params) {
         return this.store.findRecord('comment', params.id);
     }
 })
