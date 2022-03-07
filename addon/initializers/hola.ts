1e617d9cce603439697b503e51c68723b24fa379
betocantu93 <betocantu93@gmail.com>
2022-03-07 09:48:29 -0600

    fix: remove hola.ts and add qunit-dom

--- addon/initializers/hola.ts
+++
@@ -1,9 +0,0 @@
-// import Application from '@ember/application';
-
-export function initialize(): void {
-  // application.inject('route', 'foo', 'service:foo');
-}
-
-export default {
-  initialize
-};
